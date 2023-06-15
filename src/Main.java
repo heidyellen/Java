b255f04381394c8d143daa72f34478fc85405b4f
Heidyellen Costa <98487020+heidyellen@users.noreply.github.com>
2023-06-14 23:03:39 -0300

    Mensagem saudação

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
@@ -14,7 +15,23 @@ public class Main {
         Locale brasil = new Locale("pt", "BR");
         //Locale brasil = new Locale("pt", "BR");
         System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil));
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        String saudacao;
+        LocalDateTime agora = LocalDateTime.now();
+        if (agora.getHour() >=0 && agora.getHour() < 12){
+            saudacao = "Bom dia";
+        } else if (agora.getHour() >=12 && agora.getHour() <18) {
+            saudacao = "Boa tarde";
+        } else if (agora.getHour() >=18 && agora.getHour() <24) {
+            saudacao = "Boa noite";
+        } else {
+            saudacao = "";
+        }
 
+        System.out.printf("Olá, %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao.toUpperCase());
+
+        // %s representar strings
+        // %n quebra de linha
 
     }
 }
