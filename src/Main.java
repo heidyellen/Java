fdd42e051d7e09f5141f7a5b38a32012145a2548
Heidyellen Costa <98487020+heidyellen@users.noreply.github.com>
2023-06-15 01:09:59 -0300

    Funções = Soma

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,12 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        String nomeOriginal = "Let´s Code";
-        saudacao(nomeOriginal);
-    }
-    public static void saudacao(String nomeParametro) {
-        System.out.println("Hello, " + nomeParametro);
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
 
+    }
+
+    public static  int soma( int a, int b){
+        return a + b;
     }
 }
