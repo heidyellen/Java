3edcd9651d7f76e826ed877257fd300108b12487
Heidyellen Costa <98487020+heidyellen@users.noreply.github.com>
2023-06-15 00:41:12 -0300

    Arrays String

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,17 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
 
-        int numero1 = 1;
-        int numero2 = 2;
-        int numero3 = 3;
-        int numero4 = 4;
-        int numero5 = 5;
-
-        int[] numeros = new int[5];
+        String[] letras = new String[5];
         // [0] [1] [2] [3] [4]
 
-        numeros[0] = 1;
-        numeros[1] = 2;
-        numeros[2] = 3;
-        numeros[3] = 4;
-        numeros[4] = 5;
-        for (int i=0; i< numeros.length; i++) {
+        letras[0] = "A";
+        letras[1] = "B";
+        letras[2] = "C";
+        letras[3] = "D";
+        letras[4] = "E";
+        for (int i=0; i< letras.length; i++) {
 
-            System.out.println(numeros[i]);
+            System.out.println(letras[i]);
         }
     }
 }
