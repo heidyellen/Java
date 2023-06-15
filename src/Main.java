4c8c6d61a720b9a3704c151b4d9be2275f9cf444
Heidyellen Costa <98487020+heidyellen@users.noreply.github.com>
2023-06-15 00:45:00 -0300

    Arrays String com FOR

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,21 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
 
-        String[] letras = new String[5];
-        // [0] [1] [2] [3] [4]
-
-        letras[0] = "A";
-        letras[1] = "B";
-        letras[2] = "C";
-        letras[3] = "D";
-        letras[4] = "E";
-        for (int i=0; i< letras.length; i++) {
-
+//        String[] letras = new String[5];
+//        // [0] [1] [2] [3] [4]
+//
+//        letras[0] = "A";
+//        letras[1] = "B";
+//        letras[2] = "C";
+//        letras[3] = "D";
+//        letras[4] = "E";
+//        for (int i=0; i< letras.length; i++) {
+//
+//            System.out.println(letras[i]);
+//        }
+//    }
+        String[] letras = {"A", "B", "C", "D", "E"};
+        for (int i = 0; i < letras.length; i++) {
             System.out.println(letras[i]);
         }
     }
