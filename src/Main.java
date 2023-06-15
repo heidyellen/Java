8ff35f876febe1efd7b247c86f0bbd8855183a61
Heidyellen Costa <98487020+heidyellen@users.noreply.github.com>
2023-06-14 22:13:31 -0300

    switch case

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        int nota = 70;
+        int nota = 100;
+        String graduacao;
 
         // se nota maior ou igual 70, então aprovado
         // if-else
@@ -17,18 +18,31 @@ public class Main {
         // A 80 B 70 C 60 D 0 GRADUAÇÕES
 
         if (nota >= 80) {
-            System.out.println(("Graduação A"));
+            graduacao = "A";
         } else if (nota < 80 && nota >= 70) {
-            System.out.println("Graduação B");
+            graduacao = "B";
         } else if (nota < 70 && nota >= 60) {
-            System.out.println("Graduação C");
+            graduacao = "C";
         } else if (nota <60 && nota >=0) {
-            System.out.println("Graduação D");
+            graduacao = "D";
         } else {
-            System.out.println("Nota Inválida!");
+            graduacao = "";
         }
 
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno aprovado");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Não aprovado");
+                break;
+            default:
+                System.out.println("Graduação Inválida");
 
+                
+        }
 
 
     }
