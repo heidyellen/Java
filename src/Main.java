63f8623c52f383ccf27dc755785713ca106e1d08
Heidyellen Costa <98487020+heidyellen@users.noreply.github.com>
2023-06-14 23:23:52 -0300

    Tabuada i++ j++ FOR

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,14 @@ public class Main {
 
         // 1 2 3 4 5 6 7 8 9 10
         // para uma variável que inicaia em 1 e vai até 10, mudando 1 por 1, faça
-        for (int i = 1; i<=50; i++){
-            System.out.println(i);
-        }
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(j + "x" + i + " = " + j  * i);
 
+                //1*1 = 1
+                //2*2 = 2
+            }
+
+        }
     }
 }
