c6b9272b6694bba24b8b25f700aabc9516a5b75a
Heidyellen Costa <98487020+heidyellen@users.noreply.github.com>
2023-06-15 00:56:38 -0300

    Arrays Media/Maior/Menor

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,23 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
 
-//        String[] letras = new String[5];
-//        // [0] [1] [2] [3] [4]
-//
-//        letras[0] = "A";
-//        letras[1] = "B";
-//        letras[2] = "C";
-//        letras[3] = "D";
-//        letras[4] = "E";
-//        for (int i=0; i< letras.length; i++) {
-//
-//            System.out.println(letras[i]);
-//        }
-//    }
-        String[] letras = {"A", "B", "C", "D", "E"};
-        for (int i = 0; i < letras.length; i++) {
-            System.out.println(letras[i]);
-        }
+        int [] numeros = {0, 18, 12, 25, 2};
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int media = 0;
 
-        System.out.println(Arrays.toString(letras));
+        for (int i=0; i < numeros.length; i++){
+            if (numeros[i] > maior){
+                maior = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+            media += numeros[i];
+        }
+        System.out.println("Maior:" + maior);
+        System.out.println("Menor:" + menor);
+        System.out.println("Média:" + media/numeros.length);
+
     }
 }
